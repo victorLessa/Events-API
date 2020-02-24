23507b52f7a3c738ef812d92617d067224a672c5
Camara <victordsgnr@gmail.com>
2020-02-24 12:35:11 -0300

    feat: teste para modulo de usuario

--- test/functional/user.spec.js
+++ test/functional/user.spec.js
@@ -1,7 +1,38 @@
-'use strict'
+const { test, trait } = use('Test/Suite')('User')
+const User = use('App/Models/User')
+const ace = require('@adonisjs/ace')
+const Suite = use('Test/Suite')('User')
 
-const { test } = use('Test/Suite')('User')
+const { before, beforeEach, after, afterEach } = Suite
 
-test('make sure 2 + 2 is 4', async ({ assert }) => {
-  assert.equal(2 + 2, 4)
+trait('Test/ApiClient')
+
+test('SignUp User', async ({ client }) => {
+  const response = await client
+    .post('/users')
+    .send({
+      email: 'victordsgnr@gmail.com',
+      username: 'Victor',
+      password: '123',
+    })
+    .end()
+
+  response.assertStatus(200)
+})
+
+test('SignIn User', async ({ client }) => {
+  const response = await client
+    .post('/signIn')
+    .send({
+      email: 'victordsgnr@gmail.com',
+      password: '123',
+    })
+    .end()
+
+  response.assertJSONSubset({
+    id: 1,
+    username: 'Victor',
+    email: 'victordsgnr@gmail.com',
+  })
+  response.assertStatus(200)
 })
